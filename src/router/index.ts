3b7fb62204828a18fc78a74e5e39c131734e4438
danielzhang183 <danielzhang182@gmail.com>
2022-07-14 00:10:30 +0800

    feat: upload video

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import AppLayout from '@/layouts/AppLayout.vue'
+import { store } from '@/store'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,12 @@ const routes: Array<RouteRecordRaw> = [
         path: '',
         name: 'home',
         component: () => import('@/views/home/index.vue'),
+      },
+      {
+        path: 'profile',
+        name: 'profile',
+        component: () => import('@/views/profile/index.vue'),
+        meta: { requiresAuth: true },
       },
     ],
   },
@@ -25,6 +32,24 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const { user } = store.state
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!user) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath },
+      })
+    }
+    else {
+      next()
+    }
+  }
+  else {
+    next()
+  }
 })
 
 export default router
